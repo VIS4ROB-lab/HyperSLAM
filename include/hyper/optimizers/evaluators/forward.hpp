d06268d2896873d3c6675be91454bd89a591cfe2
David Hug <david.hug@mavt.ethz.ch>
2022-07-28 10:05:46 +0200

    Added further forward declarations

---
+++ include/hyper/optimizers/evaluators/forward.hpp
@@ -0,0 +1,22 @@
+/// This file is subject to the terms and conditions defined in
+/// the 'LICENSE' file, which is part of this repository.
+
+#pragma once
+
+namespace hyper {
+
+class AbstractEvaluator;
+
+template <typename>
+class Evaluator;
+
+template <typename, typename>
+struct EvaluatorQuery;
+
+template <typename>
+struct EvaluatorPointers;
+
+template <typename>
+struct EvaluatorIndices;
+
+} // namespace hyper
